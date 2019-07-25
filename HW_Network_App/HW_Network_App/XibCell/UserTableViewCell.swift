fb4d922e60a9c6f3b4f61aa96335fb18a34db060
Ксюша Жданова <zhdanovaksysha@gmail.com>
2019-07-26 01:30:24 +0300

    -- added User xib cell

--- HW_Network_App/HW_Network_App/XibCell/UserTableViewCell.swift
+++ HW_Network_App/HW_Network_App/XibCell/UserTableViewCell.swift
@@ -13,6 +13,11 @@ class UserTableViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var phoneLabel: UILabel!
     
+    func configure(_ user: User) {
+        nameLabel.text = user.name
+        phoneLabel.text = user.phone
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
